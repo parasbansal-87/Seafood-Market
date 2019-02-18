f39b87241d885cc12a68f02333672098c278cf76
parasbansal-87 <parasbansal33@gmail.com>
2019-02-18 21:41:44 +0530

    Add Components

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+//implicit return happens inside the function
+// Stateless Functional Component as it only had Html inside the function.
+
+const Header = (props) => (
+  <header className="top">
+    <h1>
+      Catch
+      <span className="ofThe">
+        <span className="of">Of</span>
+        <span className="the">The</span>
+      </span>
+      Day
+    </h1>
+    <h3 className="tagline">
+      <span>{props.tagline}</span>
+    </h3>
+  </header>
+);
+
+export default Header;
